c77ba9b887fc723b401352bb6877728ec3b57bc1
nicolas <nicolas@archipels.io>
2023-01-20 18:08:05 +0100

    feat: update plugin

--- src/main/java/fr/fazdix/nclscore/Main.java
+++ src/main/java/fr/fazdix/nclscore/Main.java
@@ -34,28 +34,39 @@ import fr.fazdix.nclscore.events.moderation.PlayerChatModeration;
 public class Main extends JavaPlugin {
     public DBModeration dbModeration;
     public Statement statementMod;
-    public HashMap<UUID, LocalDateTime> hMBannedDatetimeunban = new HashMap<>(); // BANNED PLAYER & UNBAN DATETIME
-    public HashMap<UUID, LocalDateTime> hMMutedDatetimeunban = new HashMap<>(); // Muted PLAYER & UNBAN DATETIME
-    public HashMap<Player, Player> hMTpAskedAsker = new HashMap<>(); // TPA HASHMAP ASKED AS KEY ASKER AS VALUE
-    public HashMap<Player, Boolean> hMRtp = new HashMap<>(); // RTP player waiting for rtp
+    // BANNED PLAYER & UNBAN DATETIME
+    public HashMap<UUID, LocalDateTime> hMBannedDatetimeunban = new HashMap<>();
+    // Muted PLAYER & UNBAN DATETIME
+    public HashMap<UUID, LocalDateTime> hMMutedDatetimeunban = new HashMap<>();
+    // TPA HASHMAP ASKED AS KEY ASKER AS VALUE
+    public HashMap<Player, Player> hMTpAskedAsker = new HashMap<>();
+    // RTP player waiting for rtp
+    public HashMap<Player, Boolean> hMRtp = new HashMap<>();
     public boolean blockChat = false;
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
         getServer().getConsoleSender().sendMessage("§5§lNCLS§6§lCore§a : Enabled");
-        if (getConfig().getBoolean("config.activation.msg")) // /MSG COMMAND
+        // /MSG COMMAND
+        if (getConfig().getBoolean("config.activation.msg")) {
             Objects.requireNonNull(getCommand("msg")).setExecutor(new Msg(this));
-        if (getConfig().getBoolean("config.activation.clearchat")) // /CLEARCHAT COMMAND
+        }
+        // /CLEARCHAT COMMAND
+        if (getConfig().getBoolean("config.activation.clearchat")) {
             Objects.requireNonNull(getCommand("clearchat")).setExecutor(new Clearchat(this));
-        if (getConfig().getBoolean("config.activation.blockchat")) // BLOCKCHAT COMMAND
-        {
+        }
+        // BLOCKCHAT COMMAND
+        if (getConfig().getBoolean("config.activation.blockchat")) {
             getCommand("blockchat").setExecutor(new BlockChat(this));
             getServer().getPluginManager().registerEvents(new OnChatWhithBlockChat(this), this);
         }
-        if (getConfig().getBoolean("config.activation.broadcast")) // /BROADCAST COMMAND
+        // /BROADCAST COMMAND
+        if (getConfig().getBoolean("config.activation.broadcast")) {
             Objects.requireNonNull(getCommand("broadcast")).setExecutor(new Broadcast(this));
-        if (getConfig().getBoolean("config.activation.tpa")) {// /TPA /TPYES /TPNO COMMAND
+        }
+        // /TPA /TPYES /TPNO COMMAND
+        if (getConfig().getBoolean("config.activation.tpa")) {
             Objects.requireNonNull(getCommand("tpa")).setExecutor(new TeleportRequest(this));
             Objects.requireNonNull(getCommand("tpyes")).setExecutor(new TeleportRequest(this));
             Objects.requireNonNull(getCommand("tpno")).setExecutor(new TeleportRequest(this));
@@ -68,30 +79,32 @@ public class Main extends JavaPlugin {
             Objects.requireNonNull(getCommand("sun")).setExecutor(new Weathers(this));
             Objects.requireNonNull(getCommand("rain")).setExecutor(new Weathers(this));
         }
-
-        if (getConfig().getBoolean("config.activation.moderation")) { // MODERATION TOOLS
-            String host = getConfig().getString("config.parameters.moderation.database.host"); // GET ALL DATABASE
-                                                                                               // INFORMATIONS
+        // MODERATION TOOLS
+        if (getConfig().getBoolean("config.activation.moderation")) {
+            // GET ALL DATABASE INFORMATIONS
+            String host = getConfig().getString("config.parameters.moderation.database.host");
             String port = getConfig().getString("config.parameters.moderation.database.port");
             String username = getConfig().getString("config.parameters.moderation.database.username");
             String password = getConfig().getString("config.parameters.moderation.database.password");
             String databaseName = getConfig().getString("config.parameters.moderation.database.databaseName");
             String tablePrefix = getConfig().getString("config.parameters.moderation.database.tablePrefix");
-            dbModeration = new DBModeration(host, port, username, password, databaseName, tablePrefix); // INITIALIZE
-                                                                                                        // CONNECTION
-            statementMod = dbModeration.Open(); // OPEN CONNECTION to get statement
-            try { // Create Database if she doesn't exist
+            // INITIALIZE CONNECTION
+            dbModeration = new DBModeration(host, port, username, password, databaseName, tablePrefix);
+            // OPEN CONNECTION to get statement
+            statementMod = dbModeration.Open();
+            // Create Database if she doesn't exist
+            try {
                 dbModeration.createDatabase(statementMod);
             } catch (SQLException throwables) {
                 Bukkit.getConsoleSender().sendMessage(
                         "§5§lNCLS§6§lCore §c§lError while creating the database Moderation, please check your configuration file before asking for help, looking at the error bellow might help you:\n §6"
                                 + throwables.getMessage());
             }
-            Objects.requireNonNull(getCommand("kick")).setExecutor(new Kick(this)); // /kick command
+            // /kick command
+            Objects.requireNonNull(getCommand("kick")).setExecutor(new Kick(this));
+            // Add default ban
             getServer().getConsoleSender()
-                    .sendMessage("§6Adding a default kick into the database if he's not already added ..."); // Add
-                                                                                                             // default
-                                                                                                             // ban
+                    .sendMessage("§6Adding a default kick into the database if he's not already added ...");
             try {
                 dbModeration.defaulKick(statementMod);
             } catch (SQLException throwables) {
@@ -101,16 +114,15 @@ public class Main extends JavaPlugin {
             /*
              * BAN COMMAND
              */
-            Objects.requireNonNull(getCommand("ban")).setExecutor(new Ban(this)); // /BAN COMMAND
-            getServer().getPluginManager().registerEvents(new OnJoinModeration(this), this); // KICK BANNED PLAYERS ON
-                                                                                             // JOIN
-            getServer().getPluginManager().registerEvents(new OnLeaveModeration(this), this); // Disallow leave message
-                                                                                              // if the player is banned
+            // /BAN COMMAND
+            Objects.requireNonNull(getCommand("ban")).setExecutor(new Ban(this));
+            // KICK BANNED PLAYERS ON JOIN
+            getServer().getPluginManager().registerEvents(new OnJoinModeration(this), this);
+            // Disallow leave message if the player is banned
+            getServer().getPluginManager().registerEvents(new OnLeaveModeration(this), this);
+            // ADD TO THE HASHMAP UUID BANNED AND END BAN DATE
             getServer().getConsoleSender()
-                    .sendMessage("§6Getting all banned players from the database to prevent lags"); // ADD TO THE
-                                                                                                    // HASHMAP UUID
-                                                                                                    // BANNED AND END
-                                                                                                    // BAN DATE
+                    .sendMessage("§6Getting all banned players from the database to prevent lags");
             ArrayList<UUID> allUUIDBan = new ArrayList<UUID>();
             LocalDateTime tempTimeBan = null;
             try {
@@ -126,11 +138,11 @@ public class Main extends JavaPlugin {
                 }
                 this.hMBannedDatetimeunban.put(uuid, tempTimeBan);
             }
-            getServer().getConsoleSender().sendMessage("§6Done !"); // ADD TO THE HASHMAP UUID BANNED AND END BAN DATE
+            // ADD TO THE HASHMAP UUID BANNED AND END BAN DATE
+            getServer().getConsoleSender().sendMessage("§6Done !");
+            // Add default ban
             getServer().getConsoleSender()
-                    .sendMessage("§6Adding a default ban into the database if he's not already added ..."); // Add
-                                                                                                            // default
-                                                                                                            // ban
+                    .sendMessage("§6Adding a default ban into the database if he's not already added ...");
             try {
                 dbModeration.defaultBan(statementMod);
             } catch (SQLException throwables) {
@@ -138,22 +150,12 @@ public class Main extends JavaPlugin {
             }
             getServer().getConsoleSender().sendMessage("§6Done !");
 
-            /*
-             * MUTE COMMAND
-             */
-            Objects.requireNonNull(getCommand("mute")).setExecutor(new Mute(this)); // /MUTE COMMAND
-            getServer().getPluginManager().registerEvents(new PlayerChatModeration(this), this); // KICK BANNED PLAYERS
-                                                                                                 // ON JOIN
-            getServer().getConsoleSender().sendMessage("§6Getting all muted players from the database to prevent lags");// ADD
-                                                                                                                        // TO
-                                                                                                                        // THE
-                                                                                                                        // HASHMAP
-                                                                                                                        // UUID
-                                                                                                                        // BANNED
-                                                                                                                        // AND
-                                                                                                                        // END
-                                                                                                                        // BAN
-                                                                                                                        // DATE
+            // /MUTE COMMAND
+            Objects.requireNonNull(getCommand("mute")).setExecutor(new Mute(this));
+            // KICK BANNED PLAYERS ON JOIN
+            getServer().getPluginManager().registerEvents(new PlayerChatModeration(this), this);
+            // ADD TO THE HASHMAP UUID BANNED AND END BAN DATE
+            getServer().getConsoleSender().sendMessage("§6Getting all muted players from the database to prevent lags");
             ArrayList<UUID> allUUIDMute = new ArrayList<UUID>();
             LocalDateTime tempTimeMute = null;
             try {
@@ -169,11 +171,11 @@ public class Main extends JavaPlugin {
                 }
                 this.hMMutedDatetimeunban.put(uuid, tempTimeMute);
             }
-            getServer().getConsoleSender().sendMessage("§6Done !"); // ADD TO THE HASHMAP UUID BANNED AND END BAN DATE
+            // ADD TO THE HASHMAP UUID BANNED AND END BAN DATE
+            getServer().getConsoleSender().sendMessage("§6Done !");
+            // Add default ban
             getServer().getConsoleSender()
-                    .sendMessage("§6Adding a default mute into the database if he's not already added ..."); // Add
-                                                                                                             // default
-                                                                                                             // ban
+                    .sendMessage("§6Adding a default mute into the database if he's not already added ...");
             try {
                 dbModeration.defaulMute(statementMod);
             } catch (SQLException throwables) {
@@ -190,12 +192,10 @@ public class Main extends JavaPlugin {
         if (getConfig().getBoolean("config.activation.rtp")) {
             Objects.requireNonNull(getCommand("rtp")).setExecutor(new Rtp(this));
         }
-
     }
 
     @Override
     public void onDisable() {
         getServer().getConsoleSender().sendMessage("§5§lNCLS§6§lCore§c : Disabled");
     }
-
 }
